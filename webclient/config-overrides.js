1dd12f17e6ae09fa0035de10ed39cf6058bc4f5a
juice789 <qbyqby@gmail.com>
2023-02-24 17:25:51 +0100

    updated packages

--- webclient/config-overrides.js
+++ webclient/config-overrides.js
@@ -13,5 +13,23 @@ module.exports = function override(config) {
             Buffer: ['buffer', 'Buffer']
         })
     ])
+    const fileLoaderRule = getFileLoaderRule(config.module.rules);
+    if (!fileLoaderRule) {
+        throw new Error("File loader not found");
+    }
+    fileLoaderRule.exclude.push(/\.cjs$/);
     return config;
+}
+
+function getFileLoaderRule(rules) {
+    for (const rule of rules) {
+        if ("oneOf" in rule) {
+            const found = getFileLoaderRule(rule.oneOf);
+            if (found) {
+                return found;
+            }
+        } else if (rule.test === undefined && rule.type === 'asset/resource') {
+            return rule;
+        }
+    }
 }
